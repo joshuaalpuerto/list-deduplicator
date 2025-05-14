f32c6fbfff92b925267558e0f78459b656ac0b39
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2025-05-15 01:13:30 +0300

    Evaluate list from data and export json list of duplicates

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import { promises as fs } from 'fs';
+import { parseArgs } from 'node:util';
+import yoctoSpinner from 'yocto-spinner';
+import { stripNonASCII } from './vectorizations/tokenizer';
+import TFID from './vectorizations/tfidf';
+import Cosine from './similarity/cosine';
+
+async function main() {
+  // should accept args from command line
+  // npm start -- --inputFile=./data/companies.txt --outputFile=./output.json
+  const { values } = parseArgs({
+    args: process.argv.slice(2), options: {
+      inputFile: {
+        type: "string",
+      },
+      outputFile: {
+        type: "string",
+      },
+    },
+    strict: true
+  });
+
+  if (!values.inputFile || !values.outputFile) {
+    console.error('Usage: npm start -- --inputFile=./data/companies.txt --outputFile=./output.json');
+    process.exit(1);
+  }
+
+  const inputFile = values.inputFile;
+  // // Read input file
+  const raw = await fs.readFile(inputFile, 'utf-8');
+  const documents = raw.split(/\r?\n/).map(stripNonASCII).map((l => l.trim())).filter(Boolean);
+
+  const groups = [];
+  const visited = new Set();
+
+  const spinner = yoctoSpinner({ text: 'Loading…' }).start();
+  for (let i = 0; i < documents.length; i++) {
+    if (visited.has(i)) continue;
+
+    const group = [documents[i]];
+    visited.add(i);
+
+    for (let j = i + 1; j < documents.length; j++) {
+      if (visited.has(j)) continue;
+      const bagOfWords = [documents[i], documents[j]];
+      // we only need vectorization for the documents we are matching
+      const vectorization = new TFID(bagOfWords);
+      const cosine = new Cosine(vectorization);
+      const score = cosine.similarity(documents[i], documents[j]);
+
+      if (score >= 0.6) {
+        group.push(documents[j]);
+        visited.add(j);
+      }
+    }
+    spinner.text = `Processing ${i + 1} of ${documents.length}`;
+    groups.push(group);
+  }
+  spinner.stop();
+  // // export groups to json file
+  await fs.writeFile(values.outputFile, JSON.stringify(groups, null, 2));
+
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
+
+
+
