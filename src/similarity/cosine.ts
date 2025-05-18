5982a51a4989497b6f70c8b8082723f923d3c0e0
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2025-05-18 11:13:50 +0300

    Add useful comment for cosine

--- src/similarity/cosine.ts
+++ src/similarity/cosine.ts
@@ -1,5 +1,14 @@
 import { Similarity } from './types'
 
+/**
+ * Cosine similarity is a measure of the similarity between two vectors of data.
+ * It is defined as the cosine of the angle between the two vectors.
+ * 
+ * @example
+ * const cosine = new Cosine();
+ * const similarity = cosine.similarity([1, 2, 3], [4, 5, 6]);
+ * console.log(similarity); // 0.9746318461970762
+ */
 export default class Cosine implements Similarity<number[]> {
   similarity(vectorA: number[], vectorB: number[]): number {
     // Calculate dot product and magnitudes
